9be702fef859b3986f2b3f4a1d2e27e72da00d6a
Adam George Fournier <77214723+AdamGeorge4@users.noreply.github.com>
2021-05-02 11:24:54 -0700

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,14 +1,11 @@
 var pos = 0;
-
 let pageWidth = window.innerWidth;
-
 const pacArray = [
   [images/PacMan1.png, images/PacMan2.png],
   [images/PacMan3.png, images/PacMan4.png],
 ];
 
 var direction = 0;
-
 var focus = 0;
 
 function Run() {
@@ -30,7 +27,7 @@ setInterval(Run, 200);
 
 
 function checkPageBounds(direction, imgWidth, pos, pageWidth) {
-  if (direction == 0 && pos + imgWidth > pageWidth) {direction = 1};
-  if (direction == 1 && pos < 0) {direction = 0};
+  if (direction == 0 && pos + imgWidth > pageWidth) direction = 1;
+  if (direction == 1 && pos < 0) direction = 0;
   return direction;
 }
